f11052a9a65f0936aebb8c3bb055de03c0ac5fc7
labib <alif16416@gmail.com>
2024-12-24 02:16:05 +0600

    create middleware and handle possible error

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,9 +14,21 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app=express();
+app.use(express.json());
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 });
 
-app.use('/server/user',userRouter)
+app.use('/server/user',userRouter);
+app.use('/server/auth',authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error!";
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
